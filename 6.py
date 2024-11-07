ed08c42c78e05f2c95ba5ec5a157a80fb7840f30
ursuldaniel <121333816+ursuldaniel@users.noreply.github.com>
2024-11-07 23:27:40 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -40,3 +40,30 @@ for x in range(-k, k):
         dot(5)
 
 done()
+
+
+
+#####################
+from turtle import *
+
+t = Turtle()
+t.speed(0)
+t.tracer()
+k = 23
+t.left(180)
+t.forward(5*k)
+t.right(90)
+for i in range(4):
+    t.forward(14 * k)
+    t.right(120)
+
+t.right(30)
+
+t.pu()
+
+for x in range(-k/2, k):
+    for y in range(-k/2, k):
+        t.goto(x * k, y * k)
+        t.dot(1)
+
+done()
