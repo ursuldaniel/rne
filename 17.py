edffc169f4cce012b247d1d46f6386d38d3f7a95
ursuldaniel <121333816+ursuldaniel@users.noreply.github.com>
2024-11-08 00:01:56 +0300

    Update 17.py

--- 17.py
+++ 17.py
@@ -12,7 +12,15 @@ print(counter, max_sum)
 
 
 
+nums = [int(x) for x in open("17.txt")]
 
+pairs = []
+for i in range(0 ,len(nums) - 1):
+    for j in range(i + 1, len(nums)):
+        if (nums[i] + nums[j]) % 2 == 0 and (nums[i] % 31 == 0 or nums[j] % 31 == 0):
+            pairs.append(nums[i] + nums[j])
+
+print(len(pairs), max(pairs))
 
 
 
